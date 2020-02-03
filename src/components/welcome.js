c8db6b7f3a243999696c36fb1eda3c6518a3dac9
Adam <blazekadam1@gmail.com>
2020-02-03 21:29:26 +0100

    react-spring init

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,36 +2,25 @@ import { graphql, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import React, { useState, useEffect } from "react"
 import styled from "styled-components"
-import { useTransition, animated } from "react-spring"
+import { useTrail, animated } from "react-spring"
 import BackgroundImage from "gatsby-background-image"
 
 const WelcomeContainer = styled.div`
-  background-image: url("../../images/cover.jpg");
+  position: relative;
+  width: 100%;
   height: 100vh;
+  background-image: url("../../images/cover.jpg");
+  padding-top: 20rem;
 `
 
 const Content = styled.div`
   margin: 0 auto;
   max-width: 960px;
-  padding: 10rem 0;
+  padding: 5rem 0;
   font-size: 3rem;
 `
 
 const Welcome = ({ content }) => {
-  // const re = /<\s*p[^>]*>(.*?)<\s*\/\s*p>/g
-  // const paragraphs = content.split(re).filter(paragraph => {
-  //   return /^[a-z0-9]+$/.test(paragraph)
-  // })
-
-  const [show, setDisplayContent] = useState(false)
-  const transitions = useTransition(show, null, {
-    from: { opacity: 0, transform: "translate3d(0,-40px,0)" },
-    enter: { opacity: 1, transform: "translate3d(0,0px,0)" },
-    config: { tension: 220, friction: 120 },
-  })
-  useEffect(() => {
-    setTimeout(() => setDisplayContent(true), 500)
-  })
   const { desktop } = useStaticQuery(
     graphql`
       query {
@@ -46,42 +35,63 @@ const Welcome = ({ content }) => {
     `
   )
   const imageData = desktop.childImageSharp.fluid
+
+  const re = /<\s*p[^>]*>(.*?)<\s*\/\s*p>/g // get paragraphs
+  const paragraphs = content
+    .split(re)
+    .filter(paragraph => paragraph.match(/[a-zA-Z0-9_]/g)) // remove empty strings
+  const config = { mass: 5, tension: 1000, friction: 300 }
+
+  const Text = styled(animated.div)`
+    position: relative;
+    width: 100%;
+    height: 120px;
+    line-height: 120px;
+    color: palevioletred;
+    font-size: 5em;
+    font-weight: 800;
+    text-transform: uppercase;
+    will-change: transform, opacity;
+    overflow: hidden;
+    div {
+      overflow: hidden;
+    }
+  `
+
+  const [toggle, set] = useState(false)
+  const trail = useTrail(paragraphs.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    x: toggle ? 0 : 20,
+    height: toggle ? 120 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+
+  useEffect(() => {
+    setTimeout(() => set(true), 300)
+  })
   return (
     <BackgroundImage
       Tag="section"
       fluid={imageData}
       backgroundColor={`#040e18`}
-      // Title get's passed to both container and noscriptImg.
       title="gbitest"
-      // You are able to set a classId and style by wrapper (see below or
-      // https://github.com/timhagn/gatsby-background-image/#styling--passed-through-styles):
-      // classId="gbi"
-      // style={{
-      //   // Defaults are overwrite-able by setting one of the following:
-      //   // backgroundSize: '',
-      //   // backgroundPosition: '',
-      //   // backgroundRepeat: '',
-      // }}
-      // To "force" the classic fading in of every image (especially on
-      // imageData change for fluid / fixed) by setting `soft` on `fadeIn`:
-      // fadeIn={`soft`}
-      // To be able to use stacking context changing elements yourself,
-      // set this to true to disable the "opacity hack":
-      // preserveStackingContext={true}
-      // You can "safely" (look them up beforehand ; ) add other props:
       id="gbitest"
       role="img"
       aria-label="gbitest"
     >
       <WelcomeContainer>
-        {transitions.map(
-          ({ item, key, props }) =>
-            item && (
-              <animated.div key={key} style={props}>
-                <Content dangerouslySetInnerHTML={{ __html: content }} />
-              </animated.div>
-            )
-        )}
+        {trail.map(({ x, height, ...rest }, index) => (
+          <Text
+            key={paragraphs[index]}
+            style={{
+              ...rest,
+              transform: x.interpolate(x => `translate3d(0,${x}px,0)`),
+            }}
+          >
+            <animated.div style={{ height }}>{paragraphs[index]}</animated.div>
+          </Text>
+        ))}
       </WelcomeContainer>
     </BackgroundImage>
   )
