7fee298797a4111b50bbb40ecd0511469df587d4
Adam <blazekadam1@gmail.com>
2020-06-05 23:36:23 +0200

    get keys from process env

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,9 +30,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-cloudinary-image-gallery`,
       options: {
-        cloudName: "dznxs2k2a", // FIXME store to env variable
-        apiKey: 119873996435698, // FIXME store to env variable
-        apiSecret: "9LP39DlxYhQ7rfKvcraUAVeVJjg", // FIXME store to env variable
+        cloudName: process.env.CLOUDINARY_CLOUDNAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
         folders: ["gatsby"],
       },
     },
