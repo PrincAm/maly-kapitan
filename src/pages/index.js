75c07de7d537814a12c11748338cedd0be6d2b69
Adam <blazekadam1@gmail.com>
2020-07-26 16:24:08 +0200

    move window into useEffect() on mount

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PostPreview from "../components/postPreview"
 
-const isOnHomePage = window.location.pathname === "/"
-
 export default () => {
+  const [isOnHomePage, setIsOnHomePage] = useState(false)
+  useEffect(() => {
+    setIsOnHomePage(window.location.pathname === "/")
+  }, [])
+
   return (
     <Layout isOnHomePage={isOnHomePage}>
       <SEO title="Home" />
