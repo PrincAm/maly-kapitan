5e0581a3679ae7b6d9a71da31f068a4cb9fcce75
Adam <blazekadam1@gmail.com>
2020-07-27 21:06:14 +0200

    update cover

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 import styled from "styled-components"
+import Img from "gatsby-image"
 
 import logo from "../images/logo.png"
 import Hamburger from "./hamburger"
@@ -35,13 +36,13 @@ const LogoContainer = styled.div`
   text-shadow: none;
   margin: 1.45rem 0 0 5rem;
 `
-const Logo = styled.img`
+const Logo = styled(Img)`
   height: 3.2rem;
   width: 3.2rem;
   margin-bottom: 0;
 `
 const CompanyName = styled.span`
-  color: #363636;
+  color: #000;
   font-size: 1.3rem;
   font-weight: 800;
   text-transform: uppercase;
@@ -49,6 +50,20 @@ const CompanyName = styled.span`
 `
 
 const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 200) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data)
+
   const [isMenuOpened, setMenuOpened] = useState(false)
 
   useEffect(() => {
@@ -65,7 +80,7 @@ const Header = ({ siteTitle }) => {
     <HeaderContainer>
       <Link to="/">
         <LogoContainer>
-          <Logo src={logo} />
+          <Logo fluid={data.logo.childImageSharp.fluid} />
           <CompanyName>malý kapitán</CompanyName>
         </LogoContainer>
       </Link>
