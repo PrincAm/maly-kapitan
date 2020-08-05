a7db1e54d15ed222826a812348cf68522acfda26
Adam <blazekadam1@gmail.com>
2020-08-05 21:40:16 +0200

    refactor post preview styles to css file

--- src/components/postPreview.js
+++ src/components/postPreview.js
@@ -4,25 +4,7 @@ import styled from "styled-components"
 import Truncate from "react-truncate"
 import Img from "gatsby-image"
 
-const Preview = styled.div`
-  display: flex;
-  margin-bottom: 2rem;
-`
-const PreviewImg = styled(Img)`
-  flex-shrink: 0;
-`
-const Content = styled.div`
-  margin-left: 1rem;
-`
-const Title = styled.h3`
-  margin-top: 0;
-  margin-bottom: 0.2rem;
-`
-const Date = styled.div`
-  font-size: 0.9rem;
-  opacity: 0.8;
-  margin-bottom: 0.5rem;
-`
+import "../styles/PostPreview.css"
 
 const PostPreview = ({
   excerpt,
@@ -34,30 +16,32 @@ const PostPreview = ({
 }) => {
   const imageSharp = featuredMedia && featuredMedia.localFile.childImageSharp
   return (
-    <Preview>
+    <div className="post-preview-container">
       <Link to={`/post/${slug}`}>
         {imageSharp ? (
-          <PreviewImg
+          <Img
+            className="post-preview-img"
             resolutions={imageSharp.resolutions}
             key={imageSharp.resolutions.src}
           />
         ) : (
-          <PreviewImg
+          <Img
+            className="post-preview-img"
             resolutions={defaultImage.childImageSharp.resolutions}
             key={defaultImage.childImageSharp.resolutions.src}
           />
         )}
       </Link>
-      <Content>
-        <Title>
+      <div className="post-preview-content">
+        <h3 className="post-preview-title">
           <Link to={`/post/${slug}`}>{title}</Link>
-        </Title>
-        <Date>{date}</Date>
+        </h3>
+        <div className="post-preview-date">{date}</div>
         <Truncate lines={4}>
           <div dangerouslySetInnerHTML={{ __html: excerpt }} />
         </Truncate>
-      </Content>
-    </Preview>
+      </div>
+    </div>
   )
 }
 
