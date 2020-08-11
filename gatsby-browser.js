6783a4a731676876ae856e98cb6201d1c6d9e8a7
Adam <blazekadam1@gmail.com>
2020-08-11 09:10:49 +0200

    optimize site

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,11 +3,8 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-export const onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-  if (!(`IntersectionObserver` in window)) {
-    import(`intersection-observer`)
-    console.log(`# IntersectionObserver is polyfilled!`)
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`)
   }
 }
